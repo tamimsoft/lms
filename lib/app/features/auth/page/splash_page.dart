105e19dcfa595ccb504b401ed49bbcc2be0e02da
tamim <tamimh40@gmail.com>
2025-05-09 21:11:34 +0600

    removed snack bar code

--- lib/app/features/auth/page/splash_page.dart
+++ lib/app/features/auth/page/splash_page.dart
@@ -4,7 +4,6 @@ import 'package:lms/app/config/routes/routes_name.dart';
 import 'package:lms/app/core/constants/app_colors.dart';
 import 'package:lms/app/core/constants/app_strings.dart';
 import 'package:lms/app/features/auth/controller/auth_controller.dart';
-import 'package:lms/app/helper/snack_bar.dart';
 
 import 'widget/splash_screen_app_logo.dart';
 
@@ -21,9 +20,6 @@ class SplashPage extends StatelessWidget {
       navigated = true;
       Future.delayed(const Duration(seconds: 3), () {
         final isLoggedIn = controller.isAuthenticated.value;
-        if (isLoggedIn) {
-          AppSnackBar.success(context: context, message: "Welcome back!");
-        }
         Get.offAllNamed(isLoggedIn ? RoutesName.main : RoutesName.login);
       });
     });
@@ -35,7 +31,10 @@ class SplashPage extends StatelessWidget {
           children: [
             const Spacer(flex: 1),
             const AppLogoWidget(),
-            const Text(AppString.appName, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600)),
+            const Text(
+              AppString.appName,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
             const Spacer(flex: 1),
             const CircularProgressIndicator(color: AppColors.primary),
             const SizedBox(height: 16),
