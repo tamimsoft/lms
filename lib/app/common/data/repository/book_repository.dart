042a3624c6e3fcc920f93fed92efd74225410954
tamim <tamimh40@gmail.com>
2025-05-19 18:28:47 +0600

    refactor(book_repository): implement dependency injection and improve query handling
    - Add constructor injection for SupabaseDb dependency- Refactor method calls to use the injected database instance
    - Update method names to follow consistent naming conventions
    - Improve filter handling in query methods

--- lib/app/common/data/repository/book_repository.dart
+++ lib/app/common/data/repository/book_repository.dart
@@ -1,9 +1,13 @@
 import 'package:lms/app/common/data/entity/book.dart';
 import 'package:lms/app/core/services/database/online/supabase_db.dart';
 
-class BookRepository with SupabaseDb {
+class BookRepository {
+  final SupabaseDb _db;
+
+  BookRepository(this._db);
+
   Future<List<Book>> getAll() async {
-    final data = await findAll<Book>(
+    final data = await _db.findAll<Book>(
       table: DbTable.books,
       entity: Book(), // Provide a dummy instance
     );
@@ -11,26 +15,48 @@ class BookRepository with SupabaseDb {
   }
 
   Future<Book?> getById({required String id}) async {
-    return await findById<Book>(
+    return await _db.findById<Book>(
       table: DbTable.books,
       id: id,
       entity: Book(), // Provide a dummy instance
     );
   }
 
-  Future<List<Book>> getAllByIds({required List<String> ids}) async{
-    return await findByFilter<Book>(
+  Future<List<Book>> getAllByIds({required List<String> ids}) async {
+    return await _db.findAll<Book>(
       table: DbTable.books,
-      filter: Filter(column: 'id', operator: FilterType.inFilter, value: ids),
-      entity: Book(), // Provide a dummy instance
-    );
-  }
-  Future<List<Book>> getAllByTageId({required String tageId}) async{
-    return await findByFilter<Book>(
-      table: DbTable.books,
-      filter: Filter(column: 'tage_ids', operator: FilterType.contains, value: [tageId]),
+      filters: [
+        Filter(column: 'id', operator: FilterType.inFilter, value: ids),
+      ],
       entity: Book(), // Provide a dummy instance
     );
   }
 
+  Future<List<Book>> getAllByTageId({required String tageId}) async {
+    return await _db.findAll<Book>(
+      table: DbTable.books,
+      filters: [
+        Filter(
+          column: 'tage_ids',
+          operator: FilterType.contains,
+          value: [tageId],
+        ),
+      ],
+      entity: Book(), // Provide a dummy instance
+    );
+  }
+
+  Future<List<Book>> getAllByCategoryId({required String categoryId}) async {
+    return await _db.findAll<Book>(
+      table: DbTable.books,
+      filters: [
+        Filter(
+          column: 'category_ids',
+          operator: FilterType.contains,
+          value: [categoryId],
+        ),
+      ],
+      entity: Book(), // Provide a dummy instance
+    );
+  }
 }
