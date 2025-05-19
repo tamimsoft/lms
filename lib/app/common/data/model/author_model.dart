6ddd3279fcb847e24c6b596b0ac71273d11248d9
tamim <tamimh40@gmail.com>
2025-05-19 18:35:42 +0600

    refactor: remove unused models and services
    - Remove AuthorModel, RatingModel, ReviewModel, and SlideModel classes
    - Remove SlideService class and its dependencies
    - Delete related files and imports

--- lib/app/common/data/model/author_model.dart
+++
@@ -1,7 +0,0 @@
-class AuthorModel {
-  String? id;
-  String? name;
-  String? slug;
-
-  AuthorModel({this.id, this.name, this.slug});
-}
--- lib/app/common/data/model/rating_model.dart
+++
@@ -1,9 +0,0 @@
-class RatingModel {
-  String? id;
-  String? userId;
-  String? bookId;
-  int? rating;
-  String? createdAt;
-
-  RatingModel({this.id, this.userId, this.bookId, this.rating, this.createdAt});
-}
