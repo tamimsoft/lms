e040beed1709048e1709083dce7efb3b77d94abe
tamim <tamimh40@gmail.com>
2025-05-19 18:38:01 +0600

    refactor(supabase_db): implement repository pattern and add new functionalities- Update SupabaseDb to an interface class - Add new methods: insert, update, delete, findById, findAll, findByFilter - Implement Filter and DbTable enums - Remove unused code and simplify existing implementations

--- lib/app/core/services/database/online/supabase_db.dart
+++ lib/app/core/services/database/online/supabase_db.dart
@@ -1,14 +1,58 @@
+// Define enums for database tables and filter types
+
 import 'dart:io';
 
 import 'package:lms/app/common/data/entity/base_entity.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class SupabaseDb {
+  Future<List<T>> findAll<T extends BaseEntity<T>>({
+    required DbTable table,
+    required T entity,
+    List<Filter>? filters,
+    int limit,
+    int offset,
+  });
+
+  Future<T?> findById<T extends BaseEntity<T>>({
+    required DbTable table,
+    required String id,
+    required T entity,
+  });
+
+  Future<void> insert({
+    required DbTable table,
+    required Map<String, dynamic> data,
+  });
+
+  Future<void> update({
+    required DbTable table,
+    required String id,
+    required Map<String, dynamic> data,
+  });
+
+  Future<void> delete({required DbTable table, required String id});
+
+  Future<String?> uploadImage({required File imageFile});
+}
+
+final class Filter {
+  final String column;
+  final FilterType operator;
+  final dynamic value;
+
+  const Filter({
+    required this.column,
+    required this.operator,
+    required this.value,
+  });
+}
 
 enum DbTable {
   users('user_profiles'),
   slides('slides'),
   books('books'),
-  categories('categories'), // e.g: 'action',
-  tags('tags'), // e.g: 'New Arrival',
+  categories('categories'),
+  tags('tags'),
   authors('authors'),
   publications('publications'),
   loans('loans'),
@@ -22,159 +66,17 @@ enum DbTable {
 
 enum FilterType {
   inFilter,
-  eqFilter,
-  gtFilter,
-  gteFilter,
-  ltFilter,
-  lteFilter,
-  likeFilter,
-  notInFilter,
-  notEqFilter,
-  notLikeFilter,
-  notILikeFilter,
-  notILike,
+  eq,
+  gt,
+  gte,
+  lt,
+  lte,
+  like,
+  likeAllOf,
+  likeAnyOf,
+  ilike,
+  ilikeAllOf,
+  ilikeAnyOf,
   contains,
-  overlaps
-}
-
-class Filter {
-  final String column;
-  final FilterType operator;
-  final dynamic value;
-
-  const Filter({
-    required this.column,
-    required this.operator,
-    required this.value,
-  });
-}
-
-abstract mixin class SupabaseDb {
-  SupabaseClient supabase = Supabase.instance.client;
-
-  Future<List<T>> findAll<T extends BaseEntity<T>>({
-    required DbTable table,
-    required T entity,
-    int limit = 10, // default value
-    int offset = 0, // default value
-  }) async {
-    try {
-      final response = await supabase
-          .from(table.name)
-          .select()
-          .range(offset, offset + limit - 1);
-
-      final data = List<Map<String, dynamic>>.from(response);
-      return data.map((item) => entity.fromJson(json: item)).cast<T>().toList();
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-  Future<List<T>> findByFilter<T extends BaseEntity<T>>({
-    required DbTable table,
-    required T entity,
-    required Filter filter,
-    int limit = 10,
-    int offset = 0,
-  }) async {
-    try {
-      var query = supabase.from(table.name).select();
-
-      switch (filter.operator) {
-        case FilterType.inFilter:
-          final list = List<dynamic>.from(filter.value);
-          query = query.inFilter(filter.column, list);
-          break;
-        case FilterType.overlaps:
-          final list = List<dynamic>.from(filter.value);
-          query = query.overlaps(filter.column, list);
-          break;
-        case FilterType.contains:
-          query = query.contains(filter.column, filter.value);
-          break;
-        case FilterType.eqFilter:
-          query = query.eq(filter.column, filter.value);
-          break;
-        case FilterType.gtFilter:
-          query = query.gt(filter.column, filter.value);
-          break;
-        case FilterType.gteFilter:
-          query = query.gte(filter.column, filter.value);
-          break;
-        case FilterType.ltFilter:
-          query = query.lt(filter.column, filter.value);
-          break;
-        case FilterType.likeFilter:
-          query = query.like(filter.column, filter.value);
-          break;
-        default:
-          query = query.lt(filter.column, filter.value);
-      }
-
-       query.range(offset, offset + limit - 1);
-
-      final response = await query;
-      final data = List<Map<String, dynamic>>.from(response);
-      return data.map((item) => entity.fromJson(json: item)).cast<T>().toList();
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-
-  Future<T?> findById<T extends BaseEntity<T>>({
-    required DbTable table,
-    required String id,
-    required T entity,
-  }) async {
-    try {
-      final response =
-          await supabase.from(table.name).select().eq('id', id).single();
-      return entity.fromJson(json: response);
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-
-  Future<void> insert({
-    required DbTable table,
-    required Map<String, dynamic> data,
-  }) async {
-    try {
-      await supabase.from(table.name).insert(data);
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-
-  Future<void> update({
-    required DbTable table,
-    required String id,
-    required Map<String, dynamic> data,
-  }) async {
-    try {
-      await supabase.from(table.name).update(data).eq('id', id);
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-
-  Future<void> delete({required DbTable table, required String id}) async {
-    try {
-      await supabase.from(table.name).delete().eq('id', id);
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
-
-  Future<String?> uploadImage({required File imageFile}) async {
-    final fileName = DateTime.now().millisecondsSinceEpoch.toString();
-    final path = 'uploads/$fileName';
-    try {
-      await supabase.storage.from('images').remove(['uploads/$fileName']);
-      await supabase.storage.from('images').upload(path, imageFile);
-      return path;
-    } catch (e) {
-      throw Exception('Supabase error: $e');
-    }
-  }
+  overlaps,
 }
