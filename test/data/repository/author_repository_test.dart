541b3893ea3df9d4ae6860092fdfcea5947a97ac
tamim <tamimh40@gmail.com>
2025-05-19 18:27:39 +0600

    test(author_repository): add unit tests for AuthorRepository
    - Implement unit tests for getAll(), getById(), and getAllByIds() methods
    - Use mocktail to create a mock SupabaseDb instance
    - Verify that the methods interact correctly with the database

---
+++ test/data/repository/author_repository_test.dart
@@ -0,0 +1,90 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:lms/app/common/data/entity/author.dart';
+import 'package:lms/app/common/data/repository/author_repository.dart';
+import 'package:lms/app/core/services/database/online/supabase_db.dart';
+
+class MockSupabaseDb extends Mock implements SupabaseDb {}
+
+class FakeAuthor extends Fake implements Author {}
+
+void main() {
+  late MockSupabaseDb mockDb;
+  late AuthorRepository repository;
+
+  final mockAuthors = [
+    Author(id: '1', name: 'Author 1'),
+    Author(id: '2', name: 'Author 2'),
+    Author(id: '3', name: 'Author 3'),
+  ];
+
+  setUpAll(() {
+    registerFallbackValue(FakeAuthor());
+  });
+
+  setUp(() {
+    mockDb = MockSupabaseDb();
+    repository = AuthorRepository(mockDb);
+  });
+
+  group('AuthorRepository Tests', () {
+    test('getAll() returns list of authors', () async {
+      when(() => mockDb.findAll<Author>(
+        table: DbTable.authors,
+        entity: any(named: 'entity'),
+        filters: any(named: 'filters'),
+        limit: any(named: 'limit'),
+        offset: any(named: 'offset'),
+      )).thenAnswer((_) async => mockAuthors);
+
+      final result = await repository.getAll();
+      expect(result, mockAuthors);
+      verify(() => mockDb.findAll<Author>(
+        table: DbTable.authors,
+        entity: any(named: 'entity'),
+        filters: any(named: 'filters'),
+        limit: any(named: 'limit'),
+        offset: any(named: 'offset'),
+      )).called(1);
+    });
+
+    test('getById() returns a single author', () async {
+      final author = mockAuthors.first;
+      when(() => mockDb.findById<Author>(
+        table: DbTable.authors,
+        id: any(named: 'id'),
+        entity: any(named: 'entity'),
+      )).thenAnswer((_) async => author);
+
+      final result = await repository.getById(id: '1');
+      expect(result, author);
+      verify(() => mockDb.findById<Author>(
+        table: DbTable.authors,
+        id: any(named: 'id'),
+        entity: any(named: 'entity'),
+      )).called(1);
+    });
+
+    test('getAllByIds() returns authors with matching ids', () async {
+      final ids = ['1', '2'];
+      when(() => mockDb.findAll<Author>(
+        table: DbTable.authors,
+        filters: any(named: 'filters'),
+        entity: any(named: 'entity'),
+        limit: any(named: 'limit'),
+        offset: any(named: 'offset'),
+      )).thenAnswer((_) async => mockAuthors);
+
+      final result = await repository.getAllByIds(ids: ids);
+      expect(result, mockAuthors);
+      verify(() => mockDb.findAll<Author>(
+        table: DbTable.authors,
+        filters: any(named: 'filters'),
+        entity: any(named: 'entity'),
+        limit: any(named: 'limit'),
+        offset: any(named: 'offset'),
+      )).called(1);
+    });
+  });
+}
+
