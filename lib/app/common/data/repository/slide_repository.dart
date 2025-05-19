0fe59065fe80871d37d7faa335750857b6fc3ac9
tamim <tamimh40@gmail.com>
2025-05-19 18:38:26 +0600

    refactor(slide_repository): implement dependency injection
    - Remove SlideRepository's inheritance from SupabaseDb
    - Add SupabaseDb as a dependency through SlideRepository's constructor
    - Update getAll() method to use the injected SupabaseDb instance

--- lib/app/common/data/repository/slide_repository.dart
+++ lib/app/common/data/repository/slide_repository.dart
@@ -1,9 +1,13 @@
 import 'package:lms/app/common/data/entity/slide.dart';
 import 'package:lms/app/core/services/database/online/supabase_db.dart';
 
-class SlideRepository with SupabaseDb {
+class SlideRepository {
+  final SupabaseDb _db;
+
+  SlideRepository(this._db);
+
   Future<List<Slide>> getAll() async {
-    final data = await findAll<Slide>(
+    final data = await _db.findAll<Slide>(
       table: DbTable.slides,
       entity: Slide(), // Provide a dummy instance
     );
