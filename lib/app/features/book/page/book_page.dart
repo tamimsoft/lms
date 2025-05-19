6f04cb19cbeeee9496f5b74f54d0abdbe12b6206
tamim <tamimh40@gmail.com>
2025-05-19 18:28:38 +0600

    feat(book): implement category-wise book layout
    - Add CategoryWiseBookController to manage book data
    - Implement CategoryRow and CategoryWiseBookGrid widgets
    - Include GenericSearchBar for search functionality
    - Update BookPage to display categories and corresponding books

--- lib/app/features/book/page/book_page.dart
+++ lib/app/features/book/page/book_page.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lms/app/common/widget/shimmer_placeholder.dart';
+import 'package:lms/app/features/book/controller/category_wise_book_controller.dart';
+import 'package:lms/app/features/home/page/widget/category_row.dart';
+import 'package:lms/app/features/home/page/widget/generic_search_bar.dart';
 
 import '../../home/controller/category_controller.dart';
+import 'widget/category_wise_book_grid.dart';
 
 class BookPage extends GetView<CategoryController> {
   const BookPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Get.put(CategoryController());
-    controller.fetchCategories();
+    final _ = Get.put(CategoryController());
+    final bookController = Get.put(CategoryWiseBookController());
+
     return Scaffold(
-      body: Text('data')
+      body: Column(
+        spacing: 16,
+        children: [
+          const SizedBox.shrink(),
+          GenericSearchBar(),
+          CategoryRow(
+            onTap: (categoryId) {
+              bookController.fetchBooks(categoryId: categoryId);
+            },
+          ),
+          CategoryWiseBookGrid(
+            categoryId: bookController.selectedCategoryId.value,
+          ),
+        ],
+      ),
     );
   }
 }
