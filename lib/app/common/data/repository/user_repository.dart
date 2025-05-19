9bf87e04cc2fd08d3d548c76f282c96ef00f6193
tamim <tamimh40@gmail.com>
2025-05-19 18:36:07 +0600

    refactor(user_repository): implement dependency injection and improve code structure
    - Add constructor injection for SupabaseDb
    - Update method calls to use the injected database instance
    - Modify filter parameters to use a list of filters
    - Improve code readability and maintainability

--- lib/app/common/data/repository/user_repository.dart
+++ lib/app/common/data/repository/user_repository.dart
@@ -1,9 +1,13 @@
 import 'package:lms/app/common/data/entity/user.dart';
 import 'package:lms/app/core/services/database/online/supabase_db.dart';
 
-class UserRepository with SupabaseDb {
+class UserRepository {
+  final SupabaseDb _db;
+
+  UserRepository(this._db);
+
   Future<List<User>> getAll() async {
-    final data = await findAll<User>(
+    final data = await _db.findAll<User>(
       table: DbTable.users,
       entity: User(), // Provide a dummy instance
     );
@@ -11,7 +15,7 @@ class UserRepository with SupabaseDb {
   }
 
   Future<User?> getById({required String id}) async {
-    final data = await findById<User>(
+    final data = await _db.findById<User>(
       table: DbTable.users,
       id: id,
       entity: User(),
@@ -21,19 +25,27 @@ class UserRepository with SupabaseDb {
   }
 
   Future<List<User>> getAllByIds({required List<String> ids}) async {
-    return await findByFilter<User>(
+    return await _db.findAll<User>(
       table: DbTable.users,
-      filter: Filter(column: 'id', operator: FilterType.inFilter, value: ids),
+      filters: [
+        Filter(column: 'id', operator: FilterType.inFilter, value: ids),
+      ],
       entity: User(), // Provide a dummy instance
     );
   }
 
- Future<bool> isFavoriteBook({required String bookId}) async{
-    final data = await findByFilter<User>(
+  Future<bool> isFavoriteBook({required String bookId}) async {
+    final data = await _db.findAll<User>(
       table: DbTable.users,
-      filter: Filter(column: 'favorite_book_ids', operator: FilterType.contains, value: [bookId]),
+      filters: [
+        Filter(
+          column: 'favorite_book_ids',
+          operator: FilterType.contains,
+          value: [bookId],
+        ),
+      ],
       entity: User(), // Provide a dummy instance
     );
     return data.isNotEmpty;
- }
+  }
 }
